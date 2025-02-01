88e23ac5d53009881f406b172f3b5104a515d13f
israil-fakir <israilfakirrobin@gmail.com>
2025-02-01 16:01:40 +0600

    function related problems add

---
+++ Write a python program to find the sum of the numbers passed as parameters..py
@@ -0,0 +1,8 @@
+def sum_function(x, y):
+    return x+y
+
+print("Enter the two number to find the summation")
+a = int(input("Enter a number :"))
+b = int(input("Enter a another number :"))
+
+print("The summation :", sum_function(a,b))
