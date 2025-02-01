b868e2324151d6e97861c1343e54fe05f6f63f66
israil-fakir <israilfakirrobin@gmail.com>
2025-02-01 23:34:53 +0600

    Remove the build-in function

--- Write a python program to find the smallest number from a set Of numbers.py
+++ Write a python program to find the smallest number from a set Of numbers.py
@@ -1,14 +1,13 @@
-number = [2,3,5,74,2,1]  #6
+number = [2,3,5,74,2] 
+# number.sort()
 
-# for i in number:
-#     for j in range(i+1, number):
-#         if(number[i] > number[j]):
-#             temp = number[i]
-#             number[i] = number[j]
-#             number[j] = number[i]
-
-number.sort()
-
+temp =0
+for i in range(len(number)):
+    for j in range(i+1, len(number)):
+        if(number[i] > number[j]):
+            temp = number[i]
+            number[i] = number[j]
+            number[j] = temp
 print(number[0])
 
  
