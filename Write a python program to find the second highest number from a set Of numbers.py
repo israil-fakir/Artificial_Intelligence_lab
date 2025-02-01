6328bf64df1f83ba639ca83c9972ca1976927bae
israil-fakir <israilfakirrobin@gmail.com>
2025-02-01 23:31:29 +0600

    Remove the build-in function

--- Write a python program to find the second highest number from a set Of numbers.py
+++ Write a python program to find the second highest number from a set Of numbers.py
@@ -1,17 +1,16 @@
-number = [2,3,5,74,2,1]  #6
+number = [2,3,5,4,2,1]  #6
 
 
-number.sort()
-number.reverse()
- 
+# number.sort()
+# number.reverse()
+temp = 0
+for i in range(len(number)):
+    for j in range(i+1, len(number)):
+        if(number[i] < number[j]):
+            temp = number[i]
+            number[i] = number[j]
+            number[j] = temp
 
 print(number[1])
 
-# for i in number:
-#     for j in range(i+1, number):
-#         if(number[i] > number[j]):
-#             temp = number[i]
-#             number[i] = number[j]
-#             number[j] = number[i]
-
-        
+ 
