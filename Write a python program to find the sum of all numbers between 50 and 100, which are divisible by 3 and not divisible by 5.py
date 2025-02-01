9657ddf524745db45d55a12abf6625467136a55a
israil-fakir <israilfakirrobin@gmail.com>
2025-02-02 00:41:34 +0600

    courrect the code

--- Write a python program to find the sum of all numbers between 50 and 100, which are divisible by 3 and not divisible by 5.py
+++ Write a python program to find the sum of all numbers between 50 and 100, which are divisible by 3 and not divisible by 5.py
@@ -1,8 +1,11 @@
-s = 50 
+s = 50
 f = 100
 
-print("The values: ", end="")
-for i in range(s, f+1):
-    #print(i)
-    if(i % 3 == 0 and i % 5 != 0 ):
-        print(i,end=" ")
+print("The Summation: ", end="")
+total_sum = 0
+for i in range(s, f + 1):
+    if i % 3 == 0 and i % 5 != 0:        
+        total_sum = total_sum + i
+        #print("series:",i,end=" ")
+        
+print(total_sum)
